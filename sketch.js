c80726003272b457d0fc3e995dd35dd6f58df9c2
NATALIE LUSK <NKL22@fsu.edu>
2023-09-11 11:36:50 -0400

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
   createCanvas(400, 400);
+  fill("red");
+
 }
 
 function draw() {
-  background(125);
+  background(75);
+  rect(200, 200, 300, 500);
+  ellipse(30, 30, 30, );
 }
