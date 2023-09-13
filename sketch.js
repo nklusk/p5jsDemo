c51b62ebaeb8896ec8472b23af3e799780b8afdf
NATALIE LUSK <NKL22@fsu.edu>
2023-09-13 00:36:06 -0400

    all colors input

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700);
   fill("red");
+  noStroke()
 
 }
 
 function draw() {
   background(75);
-  rect(200, 200, 300, 500);
-  ellipse(30, 30, 30, );
+
+  fill(196, 59, 43);
+  rect(0, 0, 700, 700);
+  fill(238, 54, 99);
+  rect(200, 100, 400, 600);
+  fill(254, 208, 148);
+  rect(300, 200, 500, 700);
+  fill(217, 205, 60);
+  rect(400, 300, 600, 800);
 }
