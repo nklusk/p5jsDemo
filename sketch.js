7fa50bcefc872b0d7ffd9542473b0427fe4f7ffb
NATALIE LUSK <NKL22@fsu.edu>
2023-09-13 00:43:54 -0400

    completed

--- sketch.js
+++ sketch.js
@@ -8,12 +8,19 @@ function setup() {
 function draw() {
   background(75);
 
+  //red
   fill(196, 59, 43);
   rect(0, 0, 700, 700);
+  
+  //hot pink
   fill(238, 54, 99);
-  rect(200, 100, 400, 600);
+  rect(100, 200, 500, 400);
+  
+  //beige
   fill(254, 208, 148);
-  rect(300, 200, 500, 700);
+  rect(175, 280, 350, 270);
+  
+  //lime
   fill(217, 205, 60);
-  rect(400, 300, 600, 800);
+  rect(220, 350, 260, 170);
 }
